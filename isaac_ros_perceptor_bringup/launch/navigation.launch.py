53cb564a671570e6c433d2b3cfd5d7558e4840e4
Aravind Sairam <asairam@lucidbots.com>
2024-09-24 22:09:19 +0200

    nav2 with component container isolated

--- isaac_ros_perceptor_bringup/launch/navigation.launch.py
+++ isaac_ros_perceptor_bringup/launch/navigation.launch.py
@@ -4,20 +4,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import isaac_ros_launch_utils as lu
-from isaac_ros_launch_utils.all_types import *
+
 
 def generate_launch_description():
-    
     config_dir = os.path.join(get_package_share_directory('isaac_ros_perceptor_bringup'),'params')
     param_file = os.path.join(config_dir,'lavobot_nav2.yaml')
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'),'/launch','/navigation_launch.py']),
+            PythonLaunchDescriptionSource([get_package_share_directory('nav2_bringup'),'/launch','/bringup_launch.py']),
             launch_arguments={
             'use_sim_time':'false',
+            'use_localization':'False',
+            'use_composition':'True',
+            'container_name':'nav_container',
+            'slam':'False',
+            'map':"",
             'params_file': param_file}.items(),
-
         ),
     ])
